17d9d016e4dc122791d5e11c576df2882ecbfe18
supurazako <redhot30atama@gmail.com>
2025-06-26 15:30:13 +0900

    feat(lint) add prettier & eslint

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+};
